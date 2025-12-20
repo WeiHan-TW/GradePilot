3830f6df220072deaa6a6b42bdbd86fb742aa06e
weihan <weihanlai.tw@gmail.com>
2025-12-20 17:44:44 +0800

    connect:render

--- static/config.js
+++ static/config.js
@@ -1,2 +1,2 @@
 // 設定你的後端 API 網址（Render）
-window.API_BASE = "https://YOUR_RENDER_SERVICE.onrender.com"; // <-- 改成你的 Render URL
+window.API_BASE = "https://gp-backend-mis1.onrender.com"; // <-- 改成你的 Render URL
